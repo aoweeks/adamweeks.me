922207f4dcb122b72a8dd1caab8bae52ce273d2b
aoweeks <aoweeks@gmail.com>
2017-01-01 18:35:20 +0000

    Begin navlink box animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'App works!';
+
+	private backgroundOffset: number;
+
+
+	getBackgroundPosition(elemID: string = ""): string {
+		let top = elemID;
+
+		if(elemID){
+			// let element = document.getElementById('navbar');
+			// element.style.
+		}
+		return "0px -" + (this.backgroundOffset + top) + "px";
+	}
+
+	@HostListener('window:scroll', ['$event']) 
+	updateBackgroundPosition(event) {
+	  this.backgroundOffset = document.body.scrollTop;
+	}	
+
+	title = 'App works!';
+
+	debug(input){
+		console.log(input.srcElement.attributes.id.name);
+		let temp = document.getElementById(input.srcElement.attributes.id.name);
+		console.log(temp);
+	}
 }
