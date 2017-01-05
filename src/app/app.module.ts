6d9765b47735d037a11a63f9a430ccb558674135
aoweeks <aoweeks@gmail.com>
2017-01-05 11:54:35 +0000

    Implement routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { Routing } from './app.routing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ArtPageComponent } from './art-page/art-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { CodePageComponent } from './code-page/code-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ArtPageComponent } from './pages/art-page/art-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { CodePageComponent } from './pages/code-page/code-page.component';
 
 
 @NgModule({
@@ -24,6 +25,7 @@ import { CodePageComponent } from './code-page/code-page.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    Routing,
     NgbModule.forRoot()
   ],
   providers: [],
