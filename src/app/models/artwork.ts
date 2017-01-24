4cde8d579fd0c775575e2df29478d49e553e43c8
aoweeks <aoweeks@gmail.com>
2017-01-24 23:23:10 +0000

    Flesh out Artwork class

--- src/app/models/artwork.ts
+++ src/app/models/artwork.ts
@@ -1,3 +1,20 @@
 export class ArtWork {
 
+	private title: string;
+	private url: string;
+	private date: number;
+	private medium: string;
+	private references: string[];
+
+	constructor(title: string, url: string, date: number,
+				medium: string, references: string[]){
+
+		this.title = title;
+		this.url = url;
+		this.date = date;
+		this.medium = medium;
+		this.references = references;
+
+	}
+
 }
