f243eba1d75189e84640b277c1f283d203829eb8
aoweeks <aoweeks@gmail.com>
2017-03-02 23:53:54 +0000

    Switched from CSS to SASS

--- src/app/pages/art-page/art-page.component.ts
+++ src/app/pages/art-page/art-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-art-page',
   templateUrl: './art-page.component.html',
-  styleUrls: ['./art-page.component.css']
+  styleUrls: ['./art-page.component.scss']
 })
 export class ArtPageComponent implements OnInit {
 
--- src/app/pages/gallery-page/gallery-page.component.ts
+++ src/app/pages/gallery-page/gallery-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gallery-page',
   templateUrl: './gallery-page.component.html',
-  styleUrls: ['./gallery-page.component.css']
+  styleUrls: ['./gallery-page.component.scss']
 })
 export class GalleryPageComponent implements OnInit {
 
