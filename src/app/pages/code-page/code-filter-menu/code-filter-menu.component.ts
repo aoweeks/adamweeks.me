b315536622d1a19da1a5dbb7713b176d345921ad
aoweeks <aoweeks@gmail.com>
2017-03-21 22:14:58 +0000

    Generate Code Filter Menu

---
+++ src/app/pages/code-page/code-filter-menu/code-filter-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-filter-menu',
+  templateUrl: './code-filter-menu.component.html',
+  styleUrls: ['./code-filter-menu.component.scss']
+})
+export class CodeFilterMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
