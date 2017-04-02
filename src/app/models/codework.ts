8f893c4c37136fe808fd08263d14708707b6b430
aoweeks <aoweeks@gmail.com>
2017-04-02 17:56:15 +0100

    Add imports

--- src/app/models/codework.ts
+++ src/app/models/codework.ts
@@ -1,3 +1,3 @@
-export class AppSite {
+export class Codework {
 
 }
--- src/app/services/code-list.service.ts
+++ src/app/services/code-list.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { AppSite } from '../models/appsite';
+
 
 @Injectable()
 export class CodeListService {
