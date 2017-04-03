ec3c1014f5b3d90b3fbf324349f8a0a57d37be6c
aoweeks <aoweeks@gmail.com>
2017-04-03 15:35:26 +0100

    Change radar ping speed to 1.25s

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -24,7 +24,7 @@ export class AboutPageComponent implements AfterViewInit {
 
   	this.locationCircles = this.elementRef.nativeElement.querySelectorAll('.location');
 
-    let timer = Observable.timer(0,1500);
+    let timer = Observable.timer(0,1250);
   	timer.subscribe(t=> {
         this.addLocationCircle();
     });
