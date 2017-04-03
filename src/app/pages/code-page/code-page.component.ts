b2a2c508080ca8ec881e4755ad5161c4ec97fb88
aoweeks <aoweeks@gmail.com>
2017-04-03 15:16:21 +0100

    Start implementing code service functionality

--- src/app/pages/code-page/code-page.component.ts
+++ src/app/pages/code-page/code-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-import { routerTransition } from '../../router.animations';
+import { CodeListService } from '../../services/code-list.service';
+import { Codework } from '../../models/codework';
 
 @Component({
   selector: 'app-code-page',
--- src/app/services/code-list.service.ts
+++ src/app/services/code-list.service.ts
@@ -6,6 +6,34 @@ import { Codework } from '../models/codework';
 @Injectable()
 export class CodeListService {
 
-  constructor() { }
+	private codeList: Codework[] = [];
+  private codeFilters: any = {
+    platforms: {},
+    technologies: {}
+  };
+  private sortBy: any = {
+    alpha: 0,
+    chrono: 1
+  }
+
+  constructor(private http: Http) {
+  	this.http.get('assets/api/art.json')
+  	  			.subscribe(result => this.codeListPopulator( result.json() ) );
+  }
+
+
+  codeListPopulator(result: JSON){
+  	
+  	let codeworks = JSON.parse(JSON.stringify(result)).art;
+
+
+  	for(let codework of codeworks){
+  		this.codeList.push(new Codework( codework.title, codework.url,
+  		codework.images, codework.date, 
+  		codework.type, codework.platforms, codework.technologies,
+  		codework.body) );
+  	}
+  	
+  }
 
 }
